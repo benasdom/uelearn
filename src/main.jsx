394e1fef3587b76b16c3b3df489ce68113e8bde1
benasdom <ben.k.asumadu@gmail.com>
2026-07-02 16:51:39 +0000

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const MainRouter = () => {
   const [credits, setCredits] = useState(0);
   
   return (
-    <Router basename="/uelearn">
+    <Router >
     <Routes>
         <Route path="/" element={<App />} />
         <Route path="/contact" element={<Contact />} />
