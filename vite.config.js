7208e537ffa05a1537c3efc76b61f642e9eef3d7
benasdom <ben.k.asumadu@gmail.com>
2026-07-18 09:21:57 +0000

    message

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import path from "path";
+import { copyFileSync } from "fs";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/", 
-  resolve: {
+  plugins: [
+    react(),
+        {
+      name: 'copy-404',
+      closeBundle() {
+        // Copy 404.html to dist folder after build
+        copyFileSync(
+          resolve(__dirname, 'public/404.html'),
+          resolve(__dirname, 'dist/404.html')
+        );
+      }
+    }
+  ],
+  base: "./",
+    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
