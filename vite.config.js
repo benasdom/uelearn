18347a4511fdb2cb84a3614db343ae365410d2f2
benasdom <ben.k.asumadu@gmail.com>
2026-07-18 09:08:01 +0000

    m

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
-import { copyFileSync } from "fs";
-import { resolve } from "path";
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  base: "./",
-    resolve: {
+  plugins: [react()],
+  base: "/", 
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
